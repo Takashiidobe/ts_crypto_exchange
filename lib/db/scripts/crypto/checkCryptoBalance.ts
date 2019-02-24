0d0b0dec4d641ccbfd603cfdd9e0c838495c9f05
takashiidobe <idobetakashi@gmail.com>
2019-02-24 13:03:45 -0600

    changed connectionstring

--- lib/db/scripts/crypto/checkCryptoBalance.ts
+++ lib/db/scripts/crypto/checkCryptoBalance.ts
@@ -1,7 +1,12 @@
 import knex from '../../knex';
 
 const checkCryptoBalance = (id: number) => {
-  return knex('crypto').select('cryptos').where('crypto_id', '=', id).then(result => result.json({ success: true, message: 'ok' }));
+  return (
+    knex.from('crypto')
+      .select('cryptos')
+      .where('crypto_id', '=', id)
+      .then(result => result.json({ success: true, message: 'ok' }))
+  );
 };
 
 export default checkCryptoBalance;
